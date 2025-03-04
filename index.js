87ab51b3085c86440b7a25dfa4d984c21ec24618
dennissammy77 <dennissammy77@gmail.com>
2025-03-04 15:37:52 +0300

    Strings operation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = 'Welcome to Flatbook, ';
+welcomeMessage = welcomeMessage + currentUser;
+welcomeMessage = welcomeMessage + '!';
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+excitedWelcomeMessage += currentUser.toUpperCase();
+excitedWelcomeMessage =  excitedWelcomeMessage + "!";
+
+let shortGreeting = 'Welcome, ';
+shortGreeting = shortGreeting + currentUser[0];
+shortGreeting += '!';
